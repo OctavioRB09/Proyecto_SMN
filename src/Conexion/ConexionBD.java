3f38520d728f6f3f1a599fd35cb78b74bd5292a3
juanf <juanf@DESKTOP-LB55KTV>
2021-09-28 19:55:45 -0500

    Subida de conexion a mysql

---
+++ src/Conexion/ConexionBD.java
@@ -0,0 +1,40 @@
+package Conexion;
+
+import java.sql.*;
+import javax.swing.*;
+
+public class ConexionBD {
+
+	private static String url="jdbc:mysql://localhost:3306/pruebacdb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=flase&serverTimezone=UTC";
+	private static String user="root", pass="pass";
+	
+	static {
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			JOptionPane.showMessageDialog(null, "Error de conector: "+e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+	public Connection conectar() {
+
+		try {
+
+			Connection conexion = DriverManager.getConnection(url, user, pass);
+			return conexion;
+
+		} catch (SQLException e) {
+
+			JOptionPane.showMessageDialog(null, e.getMessage());
+			e.printStackTrace();
+
+		}
+
+		return null;
+
+	}
+
+}
